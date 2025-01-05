0873575c568b99f6a2c36ebc5970d995fd42e86d
My Little Pet <mylittlepetinternal@gmail.com>
2025-01-05 14:23:51 -0300

    feat: register controllers

--- controllers/auth-controllers.go
+++ controllers/auth-controllers.go
@@ -4,21 +4,21 @@ import "net/http"
 
 
 // Registrar novo usuário.
-func Register(w http.ResponseWriter, r *http.Request){
+func RegisterUser(w http.ResponseWriter, r *http.Request){
 
 }
 
 // Realizar login e obter token JWT.
-func Login(w http.ResponseWriter, r *http.Request){
+func LoginUser(w http.ResponseWriter, r *http.Request){
 
 }
 
 // Renovar token de acesso.
-func Refresh(w http.ResponseWriter, r *http.Request){
+func RefreshUser(w http.ResponseWriter, r *http.Request){
 
 }
 // Invalidar sessão.
-func Logout(w http.ResponseWriter, r *http.Request){
+func LogoutUser(w http.ResponseWriter, r *http.Request){
 
 }
 
---
+++ controllers/profile-controllers.go
@@ -0,0 +1,22 @@
+package controllers
+
+import "net/http"
+
+// Buscar informações dos usuários.
+func GetProfile(w http.ResponseWriter, r *http.Request){
+
+}
+
+
+// Registrar perfil do usuário.
+func RegisterProfile(w http.ResponseWriter, r *http.Request){
+
+}
+
+// Atualizar informações do usuário.
+func UpdateProfile(w http.ResponseWriter,r *http.Request){
+
+}
+
+
+
